564f42bce75f155d4ad6f2d287f16441dfd857e7
Alicia <alicia.sudlerd@ut.ee>
2023-07-06 00:47:59 +0300

    remove all generated test files

--- tests/CMakeFiles/BSTTest.dir/home/alicia/Documents/Leil/assignment/src/Node.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/BSTTest.dir/home/alicia/Documents/Leil/assignment/src/Node.cpp.o: \
- /home/alicia/Documents/Leil/assignment/src/Node.cpp \
- /usr/include/stdc-predef.h \
- /home/alicia/Documents/Leil/assignment/tests/../include/Node.h
