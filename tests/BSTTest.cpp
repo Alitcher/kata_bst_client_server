a560885858c1f1694f41e7fa25efa9b39174490e
Alicia <alicia.sudlerd@ut.ee>
2023-07-05 22:49:50 +0300

    implement unit test

--- tests/BSTTest.cpp
+++ tests/BSTTest.cpp
@@ -6,14 +6,29 @@
 // Define test cases for BST
 TEST(BSTTest, Insert)
 {
-    // Test code for BST
-    // ...
+    BST bst;
+    bst.insert(5);
+    bst.insert(3);
+    bst.insert(7);
+
+    // Perform assertions to check if the insertions were successful
+    ASSERT_TRUE(bst.find(5));
+    ASSERT_TRUE(bst.find(3));
+    ASSERT_TRUE(bst.find(7));
 }
 
 TEST(BSTTest, Remove)
 {
-    // Test code for BST
-    // ...
+    BST bst;
+    bst.insert(5);
+    bst.insert(3);
+    bst.insert(7);
+    bst.remove(3);
+
+    // Perform assertions to check if the removal was successful
+    ASSERT_FALSE(bst.find(3));
+    ASSERT_TRUE(bst.find(5));
+    ASSERT_TRUE(bst.find(7));
 }
 
 // Define test cases for ServerModel
